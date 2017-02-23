81ba2e285fcdaaf8e676e0e774a6090d82ca39fc
nhne <disoxc21@gmail.com>
2017-02-24 02:22:41 +0900

    message

--- neogul.js
+++ neogul.js
@@ -4,6 +4,9 @@ function rakoonify(request, sender, sendResponse){
     {
         c.click();
     }
+    else{
+        alert("여긴 아무것도 없다구~");
+    }
 }
 
 browser.runtime.onMessage.addListener(rakoonify);
