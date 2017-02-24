9186dcf5571a50237ca428659f54fda83b837e54
nhne <disoxc21@gmail.com>
2017-02-24 10:39:30 +0900

    make neogul great again

--- neogul.js
+++ neogul.js
@@ -1,12 +1,26 @@
-function rakoonify(request, sender, sendResponse){
-    var c = $(".btn_nextpage");
-    if(c.length == 1)
-    {
-        c.click();
-    }
-    else{
-        alert("여긴 아무것도 없다구~");
-    }
+var nb;
+
+$(function(){
+    $("#example_video_1").after('<button class="neogulman">스킵해 보자구</button>');
+    nb = $("button.neogulman");
+    nb.on("click", function(){
+        var video = $("video")[0];
+        if(video.paused){
+            video.play();
+        }
+        video.currentTime = video.duration - 1.0;
+        disableNB();
+        setTimeout(function(){
+            enableNB();
+            $(".btn_nextpage").click();
+        }, 1000);
+    })
+});
+
+function disableNB(){
+    nb.text("스킵중이야~").attr("disabled", "disabled");
 }
 
-browser.runtime.onMessage.addListener(rakoonify);
+function enableNB(){
+    nb.text("스킵해 보자구").removeAttr("disabled");
+}
