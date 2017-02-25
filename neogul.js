179450b641ea7d080dadf478299649c5b75b0f52
nhne <disoxc21@gmail.com>
2017-02-25 12:38:59 +0900

    changed script event

--- neogul.js
+++ neogul.js
@@ -4,18 +4,26 @@ $(function(){
     $("#example_video_1").after('<button class="neogulman">스킵해 보자구</button>');
     nb = $("button.neogulman");
     nb.on("click", function(){
+        //removing seeking event on video
+        var script = document.createElement('script');
+        script.textContent = 'player.off("seeking")';
+        (document.head||document.documentElement).appendChild(script);
+        script.parentNode.removeChild(script);
+
         var video = $("video")[0];
         if(video.paused){
             video.play();
+            setTimeout(function(){nb.click();}, 600);
+            return;
         }
         video.currentTime = video.duration - 1.0;
         disableNB();
         setTimeout(function(){
             $(".btn_nextpage").click();
-        }, 1000);
+        }, 1800);
         setTimeout(function(){
            enableNB();
-        }, 2000);      
+        }, 2200);      
     })
 });
 
