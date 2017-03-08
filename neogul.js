977fd9afdcd7443151cf56657f38d26bef9272a5
nhne <disoxc21@gmail.com>
2017-03-08 16:26:28 +0900

    need problem skipping procedure
    auto skipping

--- neogul.js
+++ neogul.js
@@ -7,28 +7,28 @@ if(document.getElementById("example_video_1") != null){
     nb = document.createElement("button");
     nb.textContent = STRING_SKIP;
     nb.setAttribute("class", "neogulman");
-    nb.addEventListener("click", function(){
-        var script = document.createElement('script');
-        script.textContent = 'player.off("seeking")';
-        (document.head||document.documentElement).appendChild(script);
-        script.parentNode.removeChild(script);
-
-        var video = document.getElementsByTagName("video")[0];
-        if(video.paused){
-            video.play();
-            setTimeout(function(){nb.click();}, 600);
-            return;
-        }
-        video.currentTime = video.duration - 1.0;
-        disableNB();
-        setTimeout(function(){
-            $(".btn_nextpage").click();
-        }, 1800);
-        setTimeout(function(){
-            enableNB();
-        }, 2200);
-    });
+    nb.addEventListener("click", skip);
     document.getElementById("example_video_1").parentNode.appendChild(nb);
+}
+
+function skip(){
+    var script = document.createElement('script');
+    script.textContent = 'player.off("seeking")';
+    (document.head||document.documentElement).appendChild(script);
+    script.parentNode.removeChild(script);
+
+    var video = document.getElementsByTagName("video")[0];
+    if(video.paused){
+        video.play();
+        setTimeout(function(){nb.click();}, 600);
+        return;
+    }
+    video.currentTime = video.duration - 1.0;
+    disableNB();
+    setTimeout(function(){
+        document.getElementsByClassName("btn_nextpage")[0].click();
+    }, 1800);
+    setTimeout(skip, 2400);
 }
 
 //question part
@@ -36,7 +36,6 @@ var data = document.querySelectorAll("[data-answer]");
 for(var index = 0; index < data.length; index++){
     var d = data[index];
     var ans = d.getAttribute("data-answer").split(",");
-    console.log(ans);
     if(isNaN(ans[0])){
         for(var i = 0; i < ans.length; i++){
             d.querySelectorAll("input[type=text]")[i].setAttribute("value", ans[i].trim());
